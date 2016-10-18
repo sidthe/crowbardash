543202036a06c75fa5ea027bc84cda1ec685a6be
rarcea <r.arcea@gmail.com>
2016-10-18 12:44:47 +0200

    fixes to tables

--- hardware/tables.py
+++ hardware/tables.py
@@ -3,13 +3,13 @@ from horizon import tables
 
 
 class HardwareTable(tables.DataTable):
-    hostname = Column('hostname', verbose_name=_('Hostname'))
-    cpu = Column('cpu', verbose_name=_('CPU'))
-    ram = Column('ram', verbose_name=_('RAM'))
-    mac_address = Column('mac_address', verbose_name=_('MAC Address'))
-    platform = Column('platform', verbose_name=_('Platform'))
+    hostname = tables.Column("hostname", verbose_name=_("Hostname"))
+    cpu = tables.Column("cpu", verbose_name=_("CPU"))
+    ram = tables.Column("ram", verbose_name=_("RAM"))
+    mac_address = tables.Column("mac_address", verbose_name=_("MAC Address"))
+    platform = tables.Column("platform", verbose_name=_("Platform"))
 
     class Meta:
-        name = 'hardware_table'
+        name = "hardware_table"
         verbose_name = _('Hardware Table')
         multi_select = False
